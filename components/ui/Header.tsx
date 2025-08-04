4e0b81bc7cea150520350dc0ad933a663534617d
David Lee <Dai180293@gmail.com>
2025-08-04 07:23:47 +0100

    removed radix ui in favor of react-aria

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,15 +1,13 @@
-import { Separator } from "radix-ui";
+import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="h-12 flex justify-between items-center px-4 border-b border-slate-50/15">
+        <header className="h-12 flex justify-between items-center border-b border-gray-6 px-2 sm:px-4">
             <div className="flex justify-center items-center gap-5">
-                <p className="font-semibold text-lg">Cipher</p>
-                <Separator.Root
-                    className="SeparatorRoot"
-                    orientation="vertical"
-                    decorative={true}
-                />
+                <div className="flex gap-2">
+                    <Image src="/assets/logo.svg" alt="logo" width={16} height={16} />
+                    <p className="font-semibold text-lg">Cipher</p>
+                </div>
             </div>
         </header>
     );
